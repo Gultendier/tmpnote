dcf2c0df51ab08d45b56d512651691760045b1fb
Lukasz <lukasz.szupka@student.fh-kiel.de>
2024-07-29 15:03:20 +0200

    localStorage added

--- script.js
+++ script.js
@@ -3,25 +3,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	const noteArea = document.getElementById('noteArea');
 	const toggleDarkModeButton = document.getElementById('toggleDarkMode');
 
-	// Load saved note from sessionStorage when the page is loaded
-	const savedNote = sessionStorage.getItem('note');
+	// Load saved note from localStorage when the page is loaded
+	const savedNote = localStorage.getItem('note');
 	if (savedNote) {
 		noteArea.value = savedNote;
 	}
 
-	// Save the note to sessionStorage whenever the content changes
+	// Save the note to localStorage whenever the content changes
 	noteArea.addEventListener('input', () => {
 		const note = noteArea.value;
-		sessionStorage.setItem('note', note);
+		localStorage.setItem('note', note);
 	});
 
+	// Toggle dark mode
 	toggleDarkModeButton.addEventListener('click', () => {
 		document.body.classList.toggle('dark-mode');
 		const isDarkMode = document.body.classList.contains('dark-mode');
-		sessionStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
+		localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
 	});
 
-	const darkModeSetting = sessionStorage.getItem('darkMode');
+	// Load dark mode setting from localStorage
+	const darkModeSetting = localStorage.getItem('darkMode');
 	if (darkModeSetting === 'enabled') {
 		document.body.classList.add('dark-mode');
 	}
